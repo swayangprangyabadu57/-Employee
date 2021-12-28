fbc0d99a11b3a27d8bbeb99c2fc328435c1355e0
swayangprangyabadu57 <91285381+swayangprangyabadu57@users.noreply.github.com>
2021-12-28 21:41:36 +0530

    A program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'.
    1_MS4TP1T1,card 4.png
    
    Write a program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'. The output should be as follows:
    
    Name Year of joining Address Robert 1994 64C- WallsStreet Sam 2000 68D- WallsStreet John 1999 26B- WallsStreet

---
+++ employee.java
@@ -0,0 +1,21 @@
+
+public class employee {
+
+	 public static void main (String args[])
+
+	 {
+
+	 System.out.println("Name, Year of joining, Salary, Address");
+
+	 System.out.println("Robert, 1994, 40000, 64C- WallsStreat");
+
+	 System.out.println("Sam, 2000, 80000, 68D- WallsStreat");
+
+	 System.out.println("John, 1999, 62000, 26B- WallsStreat");
+
+	 }
+
+	 }
+
+
+		 
